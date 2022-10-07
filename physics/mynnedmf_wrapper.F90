e8f0a04315028461bc2573c6b89d828a7017b932
Ming.Hu <Ming.Hu@noaa.gov>
2022-10-07 16:56:51 +0000

    MYNN code changes from Joe.

--- physics/mynnedmf_wrapper.F90
+++ physics/mynnedmf_wrapper.F90
@@ -83,12 +83,6 @@
             return
          end if
 
-        if (lheatstrg) then
-          errmsg = 'Logic error: lheatstrg not implemented for MYNN PBL'
-          errflg = 1
-          return
-        end if
-
       end subroutine mynnedmf_wrapper_init
 
 ! \brief This scheme (1) performs pre-mynnedmf work, (2) runs the mynnedmf, and (3) performs post-mynnedmf work
@@ -158,14 +152,15 @@ SUBROUTINE mynnedmf_wrapper_run(        &
      &  icloud_bl, do_mynnsfclay,                          &
      &  imp_physics, imp_physics_gfdl,                     &
      &  imp_physics_thompson, imp_physics_wsm6,            &
-     &  chem3d, frp, mix_chem, rrfs_smoke, fire_turb, nchem, ndvel, &
+     &  rrfs_sd, chem3d, frp, mix_chem, enh_mix,           &
+     &  nchem, ndvel, vdep, smoke_dbg,                     &
      &  imp_physics_nssl, nssl_ccn_on,                     &
      &  ltaerosol, spp_wts_pbl, spp_pbl, lprnt, huge, errmsg, errflg  )
 
 ! should be moved to inside the mynn:
      use machine,        only: kind_phys
      use bl_mynn_common, only: cp, r_d, grav, g_inv, zero, &
-         xlv, xlvcp, xlscp
+         xlv, xlvcp, xlscp, p608
      use module_bl_mynn, only: mynn_bl_driver
 
 !------------------------------------------------------------------- 
@@ -182,6 +177,7 @@ SUBROUTINE mynnedmf_wrapper_run(        &
      !smoke/chem
      integer, intent(in) :: nchem, ndvel
      integer, parameter  :: kdvel=1
+     logical, intent(in) :: smoke_dbg
 
 ! NAMELIST OPTIONS (INPUT):
      logical, intent(in) ::                                 &
@@ -206,7 +202,7 @@ SUBROUTINE mynnedmf_wrapper_run(        &
      &       imp_physics_thompson, imp_physics_gfdl,        &
      &       imp_physics_nssl,                              &
      &       spp_pbl
-      real, intent(in) ::                                   &
+      real(kind=kind_phys), intent(in) ::                   &
      &       bl_mynn_closure
 
 !TENDENCY DIAGNOSTICS
@@ -274,7 +270,7 @@ SUBROUTINE mynnedmf_wrapper_run(        &
       real(kind=kind_phys), dimension(:), intent(in) :: xmu
       real(kind=kind_phys), dimension(:,:), intent(in) :: htrsw, htrlw
       ! spp_wts_pbl only allocated if spp_pbl == 1
-      real(kind_phys), dimension(:,:),       intent(in) :: spp_wts_pbl
+      real(kind=kind_phys), dimension(:,:),       intent(in) :: spp_wts_pbl
 
      !LOCAL
       real(kind=kind_phys), dimension(im,levs) ::                        &
@@ -286,11 +282,11 @@ SUBROUTINE mynnedmf_wrapper_run(        &
       real(kind=kind_phys), allocatable :: old_ozone(:,:)
 
 !smoke/chem arrays
-      real(kind_phys), dimension(:), intent(inout) :: frp
-      logical, intent(in) :: mix_chem, fire_turb, rrfs_smoke
+      real(kind=kind_phys), dimension(:), intent(inout) :: frp
+      logical, intent(in) :: mix_chem, enh_mix, rrfs_sd
       real(kind=kind_phys), dimension(:,:,:), intent(inout) :: chem3d
+      real(kind=kind_phys), dimension(:,:  ), intent(inout) :: vdep
       real(kind=kind_phys), dimension(im)   :: emis_ant_no
-      real(kind=kind_phys), dimension(im,ndvel) :: vdep
 
 !MYNN-2D
       real(kind=kind_phys), dimension(:), intent(in) ::                  &
@@ -357,7 +353,6 @@ SUBROUTINE mynnedmf_wrapper_run(        &
 
       !initialize arrays for test
       EMIS_ANT_NO = 0.
-      vdep = 0. ! hli for chem dry deposition, 0 temporarily
 
   ! Check incoming moist species to ensure non-negative values
   ! First, create height (dz) and pressure differences (delp) 
@@ -568,11 +563,7 @@ SUBROUTINE mynnedmf_wrapper_run(        &
           do i=1,im
           !   dz(i,k)=(phii(i,k+1) - phii(i,k))*g_inv
              th(i,k)=t3d(i,k)/exner(i,k)
-          ! keep as specific humidity
-          !   qv(i,k)=qvsh(i,k)/(1.0 - qvsh(i,k))
-          !   qc(i,k)=qc(i,k)/(1.0 - qvsh(i,k))
-          !   qi(i,k)=qi(i,k)/(1.0 - qvsh(i,k))
-             rho(i,k)=prsl(i,k)/(r_d*t3d(i,k))
+             rho(i,k)=prsl(i,k)/(r_d*t3d(i,k)*(1.+p608*max(sqv(i,k),1e-8)))
              w(i,k) = -omega(i,k)/(rho(i,k)*grav)
          enddo
       enddo
@@ -590,6 +581,11 @@ SUBROUTINE mynnedmf_wrapper_run(        &
          ch(i)=0.0
          hfx(i)=hflx(i)*rho(i,1)*cp
          qfx(i)=qflx(i)*rho(i,1)
+         !filter bad incoming fluxes
+         if (hfx(i) > 1200.)hfx(i) = 1200.
+         if (hfx(i) < -500.)hfx(i) = -500.
+         if (qfx(i) > .0005)qfx(i) = 0.0005
+         if (qfx(i) < -.0002)qfx(i) = -0.0002
 
          dtsfc1(i) = hfx(i)
          dqsfc1(i) = qfx(i)*XLV
@@ -716,10 +712,10 @@ SUBROUTINE mynnedmf_wrapper_run(        &
      &             sh3d=Sh3d,sm3d=Sm3d,                                &
 !chem/smoke
      &             nchem=nchem,kdvel=kdvel,ndvel=ndvel,                &
-     &             Chem3d=chem3d,Vdep=vdep,                            &
+     &             Chem3d=chem3d,Vdep=vdep,smoke_dbg=smoke_dbg,        &
      &             FRP=frp,EMIS_ANT_NO=emis_ant_no,                    &
-     &             mix_chem=mix_chem,fire_turb=fire_turb,              &
-     &             rrfs_smoke=rrfs_smoke,                              &
+     &             mix_chem=mix_chem,enh_mix=enh_mix,                  &
+     &             rrfs_sd=rrfs_sd,                                    &
 !-----
      &             Tsq=tsq,Qsq=qsq,Cov=cov,                            & !output
      &             RUBLTEN=RUBLTEN,RVBLTEN=RVBLTEN,RTHBLTEN=RTHBLTEN,  & !output
@@ -1033,9 +1029,9 @@ SUBROUTINE mynnedmf_wrapper_run(        &
 
     implicit none
     integer,  intent(in)     :: kte
-    real, intent(in)         :: delt
-    real, dimension(kte), intent(in)     :: dp, exner
-    real, dimension(kte), intent(inout)  :: qv, qc, qi, th
+    real(kind=kind_phys), intent(in)     :: delt
+    real(kind=kind_phys), dimension(kte), intent(in)     :: dp, exner
+    real(kind=kind_phys), dimension(kte), intent(inout)  :: qv, qc, qi, th
     integer   k
     real ::  dqc2, dqi2, dqv2, sum, aa, dum
     real, parameter :: qvmin1= 1e-8,    & !min at k=1
--- physics/sgscloud_radpre.F90
+++ physics/sgscloud_radpre.F90
@@ -112,7 +112,8 @@
       real :: rhgrid,h2oliq,qsat,tem1,tem2,clwt,es,onemrh,value
 
       !Chaboureau and Bechtold (2002 and 2005)
-      real :: a, f, sigq, qmq, qt, xl, tlk, th, thl, rsl, cpm, cb_cf
+      real :: a, f, sigq, qmq, qt, xl, th, thl, rsl, cpm, cb_cf
+      real(kind=kind_phys) :: tlk
 
       !Option to convective cloud fraction
       integer, parameter :: conv_cf_opt = 0  !0: C-B, 1: X-R
@@ -188,7 +189,7 @@
               !endif
 
               if (qc(i,k) < 1.e-6 .and. cldfra_bl(i,k)>0.001) then
-                qc(i,k) = qc_bl(i,k)*cldfra_bl(i,k)
+                qc(i,k) = qc_bl(i,k)
 
                 !eff radius cloud water (microns) from Miles et al. (2007)
                 if (nint(slmsk(i)) == 1) then !land
@@ -207,7 +208,7 @@
               snow_frac = min(0.5, max((p3d(i,k)-30000.0),0.0)/140000.0)
               ice_frac  = 1.0 - snow_frac
               if (qi(i,k) < 1.e-8 .and. cldfra_bl(i,k)>0.001) then
-                qi(i,k) = ice_frac*qi_bl(i,k)*cldfra_bl(i,k)
+                qi(i,k) = ice_frac*qi_bl(i,k)
 
                 !eff radius cloud ice (microns), from Mishra et al. (2014, JGR Atmos, fig 6b)
                 if(qi(i,k)>1.E-8)clouds5(i,k)=max(173.45 + 2.14*Tc, 20.)
@@ -220,7 +221,7 @@
               endif
 
               if (qs(i,k) < 1.e-8 .and. cldfra_bl(i,k)>0.001) then
-                qs(i,k) = snow_frac*qi_bl(i,k)*cldfra_bl(i,k)
+                qs(i,k) = snow_frac*qi_bl(i,k)
 
                 !eff radius cloud ice (microns), from Mishra et al. (2014, JGR Atmos, fig 6b)
                 if(qs(i,k)>1.E-8)clouds9(i,k)=max(2.*(173.45 + 2.14*Tc), 50.)
@@ -277,10 +278,11 @@
 
                 !Partition the convective clouds into water & frozen species
                 liqfrac = min(1., max(0., (Tk-244.)/29.))
-                qc(i,k) = qc(i,k)+qci_conv(i,k)*liqfrac
+                !For now, GF mixing ratios are added elsewhere.
+                !qc(i,k) = qc(i,k)+qci_conv(i,k)*liqfrac
                 !split ice & snow 50-50%
-                qi(i,k) = qi(i,k)+0.5*qci_conv(i,k)*(1. - liqfrac)
-                qs(i,k) = qs(i,k)+0.5*qci_conv(i,k)*(1. - liqfrac)
+                !qi(i,k) = qi(i,k)+0.5*qci_conv(i,k)*(1. - liqfrac)
+                !qs(i,k) = qs(i,k)+0.5*qci_conv(i,k)*(1. - liqfrac)
 
                 !eff radius cloud water (microns)
                 if (nint(slmsk(i)) == 1) then !land
@@ -321,10 +323,15 @@
                    sigq = SQRT(sigq**2 + 1e-10)   ! combined conv + background components
                    qmq  = a * (qt - qsat)         ! saturation deficit/excess;
                                                   !   the numerator of Q1
-                   cb_cf= min(max(0.5 + 0.36 * atan(1.55*(qmq/sigq)),0.01),0.99)
+                   cb_cf= min(max(0.5 + 0.36 * atan(1.55*(qmq/sigq)),0.0),0.99)
+                   if (qci_conv(i,k) .lt. 1e-9) cb_cf = 0.0
                    if (do_mynnedmf .and. qmq .ge. 0.0) then
                       ! leverage C-B stratus clouds from MYNN in saturated conditions
-                      clouds1(i,k) = 0.5*(clouds1(i,k) + cb_cf)
+                      if (cb_cf .gt. 0.0) then
+                         clouds1(i,k) = 0.5*(clouds1(i,k) + cb_cf)
+                      else
+                         !default to MYNN clouds - already specified
+                      endif
                    else                           ! unsaturated
                       clouds1(i,k) = cb_cf
                    endif
